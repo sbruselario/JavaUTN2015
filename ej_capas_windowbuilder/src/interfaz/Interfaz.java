a7886a0a5a43f40110e2dcbf4b2d14850c6fb4ed
sbruselario <sbruselario@frro.utn.edu.ar>
2015-06-04 22:19:10 -0300

    setLookAndFeel agregado a la interfaz

--- ej_capas_windowbuilder/src/interfaz/Interfaz.java
+++ ej_capas_windowbuilder/src/interfaz/Interfaz.java
@@ -44,6 +44,11 @@ public class Interfaz extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
