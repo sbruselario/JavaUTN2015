257176c9b4de6b8c72760f1db08a378f79cb2ed5
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2015-09-24 14:39:08 -0300

    Agregada lib mysql y FactoryConexion.

---
+++ tp_ajedrez/src/datos/FactoryConexion.java
@@ -0,0 +1,70 @@
+package datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FactoryConexion {
+
+
+	//configuración para la conexión
+	String dbDriver="com.mysql.jdbc.Driver";
+	String host="localhost";
+	String port="3306";
+	String user="javaapp";
+	String pass="javaapp";
+	String db="java_ajedrez";
+
+	private Connection conn;
+	private int cantConexiones;
+
+
+	private FactoryConexion(){
+		try {
+			Class.forName(dbDriver);
+			conn=null;
+			cantConexiones=0;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	private static FactoryConexion instancia;
+	public static FactoryConexion getInstancia(){
+		if (instancia==null){
+			instancia = new FactoryConexion();
+		}
+		return instancia;
+	}
+
+
+	public Connection getConexion(){
+		try {
+			if(conn==null || conn.isClosed()){
+				conn = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db+"?user="+user+"&password="+pass);
+				cantConexiones++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+
+	public void releaseConexion(){
+		try {
+			cantConexiones--;
+			if(cantConexiones==0){
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+}
