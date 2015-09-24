11e0a78b60e12062476364477198c9333aa347ec
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2015-09-24 13:40:46 -0300

    Movimiento Rey agregado.

--- tp_ajedrez/src/entidades/Rey.java
+++ tp_ajedrez/src/entidades/Rey.java
@@ -2,4 +2,35 @@ package entidades;
 
 public abstract class Rey extends Trebejo {
 
+	private static char nombre = 'r';
+	
+	public static char getNombre() {
+		return nombre;
+	}
+
+	public static void setNombre(char nombre) {
+		Rey.nombre = nombre;
+	}
+
+
+	public boolean movimientoValido (Posicion posicion, Trebejo trebejo){
+		if(trebejo.getColor() == this.getColor()) {
+			return false;
+		}else{
+			//en todas las direcciones, de a una posición
+			if (Math.abs(this.getPosicion().getNumero() - posicion.getNumero()) == 1 &&
+					this.getPosicion().getLetra() == posicion.getLetra()){
+				return true;
+			}else if (this.getPosicion().getNumero() == posicion.getNumero()  &&
+					Math.abs(this.getPosicion().getLetra() - posicion.getLetra()) == 1){
+				return true;
+			}else if (Math.abs(this.getPosicion().getNumero() - posicion.getNumero()) == 1 &&
+					Math.abs(this.getPosicion().getLetra() - posicion.getLetra()) == 1){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+	
 }
