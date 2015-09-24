aad792049da17b74bf02f720996b88a2c55e7212
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2015-09-24 13:32:27 -0300

    Movimiento Torre agregado.

--- tp_ajedrez/src/entidades/Torre.java
+++ tp_ajedrez/src/entidades/Torre.java
@@ -2,4 +2,23 @@ package entidades;
 
 public abstract class Torre extends Trebejo {
 
+	private static char nombre = 't';
+
+	public static char getNombre() {
+		return nombre;
+	}
+
+	public static void setNombre(char nombre) {
+		Torre.nombre = nombre;
+	}
+
+	public boolean movimientoValido (Posicion posicion, Trebejo trebejo){
+		if(trebejo.getColor() == this.getColor()) {
+			return false;
+		}else{
+			//cuando el número o la letra coinciden 
+			return ((this.getPosicion().getNumero() == posicion.getNumero()) ||
+					(this.getPosicion().getLetra() == posicion.getLetra()));
+		}
+	}
 }
