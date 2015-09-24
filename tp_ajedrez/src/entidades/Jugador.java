e8aaf0b15f727416cdbc9dda45346f22c8ebb4b8
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2015-09-24 13:00:13 -0300

    Agregado Jugador.

---
+++ tp_ajedrez/src/entidades/Jugador.java
@@ -0,0 +1,26 @@
+package entidades;
+
+public class Jugador {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+}
