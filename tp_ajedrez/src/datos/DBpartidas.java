a22f518e802e0ed62e3025de5af70409617380c3
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2015-10-10 11:58:38 -0300

    Métodos agregados a DBpartidas.

--- tp_ajedrez/src/datos/DBpartidas.java
+++ tp_ajedrez/src/datos/DBpartidas.java
@@ -90,5 +90,123 @@ public class DBpartidas {
 
 
 
+	public static Partida buscarPartida(int id){
+		Connection conexion;
+		Partida partida = new Partida();
+		conexion = FactoryConexion.getInstancia().getConexion();
+		String sql1="select * from partidas where idPartida = ?";
+		String sql2="select idPieza, estado, letra, numero from piezas_partidas where idPartida = ?";
+		try {
+			PreparedStatement comando = conexion.prepareStatement(sql1);
+			comando.setInt(1, id);
+			ResultSet rs = comando.executeQuery();
+			if(rs.next()){
+				int idPartida = rs.getInt(1);
+				String dniJugador1 = rs.getString(2);
+				String dniJugador2 = rs.getString(3);
+				char turno = rs.getString(4).toCharArray()[0];
+
+				partida.setIdPartida(idPartida);
+				partida.setJ1(DBjugadores.buscarDni(dniJugador1));
+				partida.setJ2(DBjugadores.buscarDni(dniJugador2));
+				partida.setTurno(turno);
+				Trebejo[] piezas = new Trebejo[32];
+				piezas = DBpartidas.getPiezas();
+				partida.setPiezas(piezas);
+				Connection conexion2 = FactoryConexion.getInstancia().getConexion();
+				PreparedStatement comando2 = conexion2.prepareStatement(sql2);
+				comando2.setInt(1, id);
+				ResultSet rs2= comando2.executeQuery();
+				while(rs2.next())
+				{
+					int idPieza = rs2.getInt(1);
+					boolean estado = rs2.getInt(2)==1;
+					char letra = rs2.getString(3).toCharArray()[0];
+					int numero = rs2.getInt(4);
+					Trebejo pieza = partida.buscarPieza(idPieza);
+					Posicion pos = new Posicion();
+					pos.setLetra(letra);
+					pos.setNumero(numero);
+					pieza.setPosicion(pos);
+					pieza.setEliminado(estado);
+					partida.updatePieza(pieza);
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		FactoryConexion.getInstancia().releaseConexion();
+
+		return partida;
+	}
+
+
+
+
+
+
+	public static Partida nuevaPartida(Partida partidaActual) {
+		Connection conexion = FactoryConexion.getInstancia().getConexion();
+		String sql = "insert into partidas (dniJugador1, dniJugador2) values (?,?);";
+		String insert = "insert into piezas_partidas (idPieza, idPartida, letra, numero) values (?,?,?,?);";
+		PreparedStatement comando;
+		try {
+			comando = conexion.prepareStatement(sql);
+			comando.setInt(1, partidaActual.getJ1().getDni());
+			comando.setInt(2, partidaActual.getJ2().getDni());
+			comando.executeUpdate();
+			ResultSet rs = comando.getGeneratedKeys();
+			rs.next();
+			partidaActual.setIdPartida(rs.getInt(1));
+			partidaActual.setTurno('b');
+			for (Pieza pieza : partidaActual.getPiezas()) {
+				comando = conexion.prepareStatement(insert);
+				comando.setInt(1, pieza.getIdPieza());
+				comando.setInt(2, partidaActual.getIdPartida());
+				comando.setString(3, Character.toString(pieza.getPosicion().getLetra()));
+				comando.setInt(4, pieza.getPosicion().getNumero());
+				comando.executeUpdate();
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FactoryConexion.getInstancia().releaseConexion();
+		return partidaActual;	
+	}
+	public static Posicion getPosicion(int idPieza, int idPartida) {
+		Posicion p = new Posicion();
+		String sql = "select letra, numero from piezas_partidas where (idPieza = ? and idPartida = ?); ";
+		Connection conexion = FactoryConexion.getInstancia().getConexion();
+		PreparedStatement comando;
+		try {
+			comando = conexion.prepareStatement(sql);
+			comando.setInt(1, idPieza);
+			comando.setInt(2, idPartida);
+			ResultSet rs = comando.executeQuery();
+			rs.next();
+			p.setLetra(rs.getString("letra").toCharArray()[0]);
+			p.setNumero(rs.getInt("numero"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FactoryConexion.getInstancia().releaseConexion();
+		return p;
+	}
+
+
+
+
+
+
+
+
+
 
 }
