af1442dfa3f9a7869c60d14a1a80ccece206c362
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2015-10-11 09:13:59 -0300

    Cambios varios en la capa datos.

--- tp_ajedrez/src/datos/DBjugadores.java
+++ tp_ajedrez/src/datos/DBjugadores.java
@@ -1,104 +1,51 @@
 package datos;
 
 import entidades.Jugador;
+import datos.FactoryConexion;
 import java.sql.*;
+
+
 
 public class DBjugadores {
 
-
-	//para agregar un jugador a la db 
-	public String addJugador(Jugador jugador) throws ClassNotFoundException, SQLException{
-
-		ResultSet rs=null;
-		PreparedStatement stmt=null;
-
+	public static Jugador buscarJugador(int dni){
+		Jugador jugador = new Jugador();
+		Connection con;
+		con = FactoryConexion.getInstancia().getConexion();
+		String sql = "select * from jugadores where dni = ?";
 		try {
-			stmt = FactoryConexion.getInstancia().getConexion().prepareStatement(
-					"insert into jugadores(dni, nombre, apellido) values (?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
-			stmt.setString(1, jugador.getDni());
-			stmt.setString(2, jugador.getNombre());
-			stmt.setString(3, jugador.getApellido());
-			stmt.execute();
+			PreparedStatement comando = con.prepareStatement(sql);
+			comando.setInt(1, dni);
+			ResultSet rs = comando.executeQuery();
+			if(rs.next()){
+				jugador.setDni(rs.getInt(1));
+				jugador.setNombre(rs.getString(2));
+				jugador.setApellido(rs.getString(3));
 
-			rs=stmt.getGeneratedKeys();
-
-			if(rs!=null && rs.next()){
-				return (rs.getString(1));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		finally{
-			try {
-				if(rs!=null ) rs.close();
-				if(stmt != null) stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			FactoryConexion.getInstancia().releaseConexion();
-		}
-		return null;
-	}
-
-
-	//para actualizar los datos de un jugador 
-	public void updateJugador(Jugador nJugador){
-		Jugador jugador = this.buscarDni(nJugador.getDni());
-		jugador.setNombre(nJugador.getNombre());
-		jugador.setApellido(nJugador.getApellido());
-		PreparedStatement stmt=null;
-
-		try {
-			stmt = FactoryConexion.getInstancia().getConexion().prepareStatement(
-					"update jugadores set nombre = ?, apellido = ? where dni = ? ;");
-			stmt.setString(4, jugador.getDni());
-			stmt.setString(1, jugador.getNombre());
-			stmt.setString(2, jugador.getApellido());
-			stmt.execute();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally{
-			FactoryConexion.getInstancia().releaseConexion();
-		}
-	}
-
-
-
-	//para buscar un jugador por dni 
-	public static Jugador buscarDni(String dni){
-		ResultSet rs=null;
-		PreparedStatement stmt=null;
-		Jugador jugador=null;
-		try {
-			stmt = 	FactoryConexion.getInstancia().getConexion().prepareStatement(
-					"select id, dni, nombre, apellido from jugadores where dni = ?");
-			stmt.setString(1, dni);
-			rs = stmt.executeQuery();
-			if(rs !=null && rs.next()){
-				jugador = new Jugador(rs.getString("dni"),rs.getString("nombre"),rs.getString("apellido"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally
-		{
-			try {
-				if(rs!=null)rs.close();
-				if(stmt!=null) stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			FactoryConexion.getInstancia().releaseConexion();
-		}
+		FactoryConexion.getInstancia().releaseConexion();
 		return jugador;
 	}
+
+	public static void registrarJugador(Jugador jugador){
+		Connection con;
+		con = FactoryConexion.getInstancia().getConexion();
+		String sql = "insert into jugadores (dni, nombre, apellido)" +
+				"values (" + jugador.getDni() + ", '" + jugador.getNombre() + "', '" + jugador.getApellido() + "');";
+		try {
+			Statement comando = con.createStatement();
+			comando.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FactoryConexion.getInstancia().releaseConexion();
+	}
+
 
 
 
